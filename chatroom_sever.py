dddd948ccac6ae2f445f0fa4df900e30f81899db
richardqlin <richardqlin@gmail.com>
2021-09-12 14:06:00 -0700

    Update chatroom_sever.py

--- chatroom_sever.py
+++ chatroom_sever.py
@@ -12,53 +12,35 @@ s.bind((host,port))
 
 s.listen(5)
 
-clients = []
+
 
 clients_dict={}
-nicknames = []
+
 
 def broadcast(message):
-    print(clients)
-    for client in clients:
+    for c in clients_dict:
+        print(c)
+        clients_dict[c].send(message)
 
-        client.send(message)
-
-def handle(client):
+def handle(client,name):
+    global clients_dict
     while 1:
         try:
             message = client.recv(1024)
-            print(nicknames[clients.index(client)])
-
             broadcast(message)
         except:
-            index = clients.index(client)
-            clients.remove(client)
             client.close()
-            nickname= nicknames[index]
-            nicknames.remove(nickname)
+            del clients_dict[name]
             break
 
 
 def receive():
     while 1:
         client, address = s.accept()
-        print('Connection with',address,client)
-
-
-        client.send(''.encode('utf-8'))
-        nickname = client.recv(1024)
-        print('nickname',nickname)
-
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print('clients',clients_dict)
-
-        print('Nick name of the client is', nickname)
-        broadcast(f"{nickname} connected to the server\n".encode('utf-8'))
-        client.send('Connect to the server'.encode('utf-8'))
-
-        thread = threading.Thread(target=handle, args = (client,))
+        data = client.recv(1024)
+        clients_dict[data] = client
+        broadcast(data+ ' connected to the server '.encode('utf-8'))
+        thread = threading.Thread(target=handle, args = (client,data,))
         thread.start()
 
 receive()
